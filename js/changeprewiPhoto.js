6fbcbdf5f6c4dd538f5ac4dace60d7c96b41a0f4
Sergey Andreev <ozwatandree@gmail.com>
2022-06-18 17:23:59 +0300

    ChangeShowPhoto

---
+++ js/changeprewiPhoto.js
@@ -0,0 +1,17 @@
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+const fileChooser = document.querySelector('#upload-file');
+const preview = document.querySelector('#photoprewiew2');
+
+fileChooser.addEventListener('change', () => {
+  const file = fileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => {
+    return fileName.endsWith(it);
+  });
+
+  if (matches) {
+    preview.src = URL.createObjectURL(file);
+  }
+});
--- js/start.js
+++ js/start.js
@@ -5,6 +5,5 @@ import  './uiSlider.js';
 import './get-data.js';
 import './real-post-create.js';
 import './photo-submit.js';
-
-
+import './changeprewiPhoto.js';
   
